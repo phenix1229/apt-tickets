e30be7929411e16deb63b41a193e352b5427c1c2
unknown <paul.garay@codeimmersives.com>
2024-09-04 22:07:06 -0400

    Added users routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Put, Delete, Param, Body } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get()
+    public getUsers() {
+        return 'you sent get request to users endpoint';
+    }
+    
+    @Get('/:id')
+    public getUser(@Param() params: any) {
+        return 'you sent get request to users endpoint';
+    }
+
+    @Post()
+    public createUsers(@Body() request: any) {
+        console.log(request)
+        return request;
+    }
+}
